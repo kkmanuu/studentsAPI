7354c274548149656d93500a09727bb9560317e5
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-02-22 16:59:14 +0300

    initialize Express server with student routes and MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const routes = require('./routes/api');
+const routes = require('./routes/studentsRoute');
 const app = express();
+require('dotenv').config();
+require('./helpers/init_mongodb');
 
 app.listen(process.env.port || 4000 , function(){
     console.log('Now Listening for request on:http://localhost:4000')
