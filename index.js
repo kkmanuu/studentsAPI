cd300481f599342b7f10de34a5f431a540f9f69b
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-02-25 13:57:39 +0300

    feat: add express.json middleware for parsing JSON requests

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const routes = require('./routes/studentsRoute');
+const studentsroutes = require('./routes/studentsRoute');
 const app = express();
 require('dotenv').config();
 require('./helpers/init_mongodb');
+
+app.use(express.json());
+app.use("/students",studentsroutes )
 
 app.listen(process.env.port || 4000 , function(){
     console.log('Now Listening for request on:http://localhost:4000')
 });
 
-
-app.use(routes);
