a35d43045b9a751fea53644fccff9b8ab6053262
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-02-26 19:21:44 +0300

    feat: add error handling and global error handler

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ require('./helpers/init_mongodb');
 app.use(express.json());
 app.use("/students",studentsroutes )
 
+
+//handling errors
+
+app.use(async (req, res, next) =>{
+    next(createError.NotFound());
+});
+
+//Error handler
+app.use((err,req, res, next) =>{
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message:err.message,
+        },
+    });
+});
+
 app.listen(process.env.port || 4000 , function(){
     console.log('Now Listening for request on:http://localhost:4000')
 });
