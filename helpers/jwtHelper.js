f4b191fde44b61a18dc5859ff39b302cc9d48682
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-03-04 14:45:42 +0300

    Add access token verification and refresh token generation

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -3,22 +3,61 @@ const createError = require("http-errors");
 const students = require("../models/students");
 
 module.exports = {
-  signAccessToken: (userId) => {
-    return new Promise((resolve, reject) => {
-      const payload = {};
+  signAccessToken: (UserId) => {
+    return new Promise((resolve, reject) => { 
+      const payload = {}; 
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: "1h",
+        expiresIn: "2h",
         issuer: "EddTechnologies.com",
-        audience: userId,
+        audience: UserId,
       };
+  
       JWT.sign(payload, secret, options, (error, token) => {
         if (error) {
           console.log(error.message);
-          reject(createError.InternalServerError());
+          return reject(createError.InternalServerError());
         }
         resolve(token);
       });
+    });
+  },  
+  verifyAccessToken: (req, res, next) => {
+    if (!req.headers["authorization"]) {
+      return next(createError.Unauthorized());
+    }
+  
+    const bearerToken = req.headers["authorization"].split(" ")[1];
+  
+    JWT.verify(bearerToken, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+      if (err) {
+        return next(createError.Unauthorized(err.message));
+      }
+      req.payload = payload;
+      next();
+    });
+  },
+  
+  signRefreshToken: () => {
+    return new Promise((resolve, reject) => {
+      const payload = {};
+      const options = {
+        expiresIn: "1d",
+        audience: User.id,
+      };
+
+      JWT.sign(
+        payload,
+        process.env.REFRESH_TOKEN_SECRET,
+        options,
+        (err, token) => {
+          if (err) {
+            console.log(err);
+            reject(createError.InternalServerError());
+          }
+          resolve(token);
+        }
+      );
     });
   },
 };
