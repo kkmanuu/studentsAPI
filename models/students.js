d28500e79c2612cf7fcd5494017f7808a5144f12
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-03-26 13:05:35 +0300

    fix: update student schema and add role field (commented)

--- models/students.js
+++ models/students.js
@@ -1,24 +1,23 @@
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-    firstname:{
-        type:String,
-        require:[true, 'Firstname is required']
-    },
-    lastname:{
-        type:String,
-        require:[true, 'Lastname is required']
-    },
-    gender:{
-        type: String
-    },
-    // role:{
-    //     type: DataTypes.ENUM('admin', 'nan1', 'student'),
-    //     defaultValue: 'student'
-    // },
+  firstname: {
+    type: String,
+    require: [true, "Firstname is required"],
+  },
+  lastname: {
+    type: String,
+    require: [true, "Lastname is required"],
+  },
+  gender: {
+    type: String,
+  },
+  // role:{
+  //     type: DataTypes.ENUM('admin', 'nan1', 'student'),
+  //     defaultValue: 'student'
+  // },
 });
 
-
-const Student = mongoose.model('student', studentSchema);
+const Student = mongoose.model("student", studentSchema);
 module.exports = Student;
